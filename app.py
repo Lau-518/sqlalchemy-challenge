2ace7981487e7228531a31bc540de45eda90efc7
Laurent <laurentahsue888@gmail.com>
2021-12-09 11:06:54 +0800

    progress4

--- app.py
+++ app.py
@@ -7,11 +7,17 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
+#################################################
+# Latest Date
+#################################################
+
+
+query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -42,7 +48,6 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/<start>"
         f"/api/v1.0/<start>/<end>"
        
@@ -58,7 +63,8 @@ def precipitation():
 
     """Return a list of precipitaion data for every date"""
     # Query all precipation data per date
-    results = session.query(Measurement.date, Measurement.prcp).all()
+    results = session.query(Measurement.date, Measurement.prcp).\
+                  filter(Measurement.date >= query_date).all()
 
     session.close()
 
@@ -76,28 +82,101 @@ def precipitation():
 
 
 
-# @app.route("/api/v1.0/passengers")
-# def passengers():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-#     # Query all passengers
-#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    """Return a list of stations in the database"""
+    # Query all precipation data per date
+    results = session.query(Station.id, Station.station).\
+                  filter(Measurement.date >= query_date).all()
 
-#     session.close()
+    session.close()
 
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_passengers = []
-#     for name, age, sex in results:
-#         passenger_dict = {}
-#         passenger_dict["name"] = name
-#         passenger_dict["age"] = age
-#         passenger_dict["sex"] = sex
-#         all_passengers.append(passenger_dict)
-
-#     return jsonify(all_passengers)
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_Station = []
+    for id, station in results:
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+        Station_dict = {}
+        Station_dict["id"] = id
+        Station_dict["station"] = station
+        all_Station.append(Station_dict)
+
+    return jsonify(all_Station)
+
+@app.route("/api/v1.0/tobs")
+
+def temperature():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all tobs for the most active station in the database"""
+    # Query all precipation data per date
+    # results = session.query(Station.id, Station.station).all()
+
+    most_active_station = session.query(Measurement.date, Measurement.station, 
+                        Measurement.tobs).\
+                  filter(Measurement.station == "USC00519281").\
+                  filter(Measurement.date >= query_date).all()
+                    
+
+
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    Station_mostactive = []
+    for date, station, tobs in most_active_station:
+        most_active_Station_dict = {}
+        most_active_Station_dict["date"] = date
+        most_active_Station_dict["station"] = station
+        most_active_Station_dict["tobs"] = tobs
+
+        Station_mostactive.append(most_active_Station_dict)
+
+
+    return jsonify(Station_mostactive)
+
+@app.route("/api/v1.0/<start_date>")
+
+def start_date_climate(start_date):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all tobs for the most active station in the database"""
+    # Query all precipation data per date
+    # results = session.query(Station.id, Station.station).all()
+
+    start_date_climate = session.query(func.min(Measurement.tobs), 
+                       func.avg(Measurement.tobs), 
+                        func.max(Measurement.tobs)).\
+                  filter(Measurement.date >= start_date).all()
+                  
+
+
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+
+    start_date_climate = []
+    for date, station, tobs_min,tobs_avg, tobs_max  in start_date_climate:
+        start_date_climate_dict = {}
+        start_date_climate_dict["tobs_min"] = tobs_min
+        start_date_climate_dict["tobs_avg"] = tobs_avg
+        start_date_climate_dict["tobs_max"] = tobs_max
+
+
+        start_date_climate.append(start_date_climate_dict)
+
+   
+    
+
+
+    return jsonify(start_date_climate)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
